d8bb156f3939458829a567b0dd3b47107bc6095f
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-06-20 16:03:27 -0300

    requisito 3 - implementação da classe MenuData, que retorna um set com nome do prato, receita e valor

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,35 @@
+# Resolvida com ajuda da monitoria, Chat GPT e Rodrigo Monteiro (25B)
+import csv
+from models.ingredient import Ingredient
+from models.dish import Dish
+from collections import defaultdict
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path, encoding="utf8") as file:
+            menu_reader = csv.reader(file, delimiter=",", quotechar='"')
+            header, *data = menu_reader
+        self.dishes = self.parse_data(data)
+
+    def parse_data(self, data):
+        recipes = defaultdict(list)  # para não dar KeyError
+        for item in data:
+            dish = item[0]
+            price = float(item[1])
+            ing_name = item[2]
+            ing_qty = int(item[3])
+
+            if dish not in recipes:
+                recipes[dish] = Dish(dish, price)
+
+            new_ing = Ingredient(ing_name)
+            recipes[dish].add_ingredient_dependency(new_ing, ing_qty)
+        return set(recipes.values())
+
+
+# Obs.: o código abaixo NÃO funcionou para fazer o set de ingredientes:
+# new_dish.add_ingredient_dependency(new_ing, ing_qty)
+# recipes[dish].append(tuple(new_dish.recipe))
+# Erro: Tipo lista não é hasheable
